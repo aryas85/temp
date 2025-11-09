984470fb6c4f81a5451032e22aad428cf434d640
aryas85 <aryasalokhe11@gmail.com>
2025-11-09 17:58:53 +0530

    Add files via upload

---
+++ process-demo.js
@@ -0,0 +1,16 @@
+// 1️⃣ process.cwd() → shows current working directory
+console.log("Current Working Directory:", process.cwd());
+
+// 2️⃣ process.pid → gives process ID
+console.log("Process ID:", process.pid);
+
+// 3️⃣ process.version → shows Node.js version
+console.log("Node.js Version:", process.version);
+
+// 4️⃣ process.platform → shows OS platform
+console.log("Platform:", process.platform);
+
+// 5️⃣ process.memoryUsage() → shows memory used by the process
+console.log("Memory Usage:", process.memoryUsage());
+
+
